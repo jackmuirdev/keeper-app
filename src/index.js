80dc6503bbce781d5a5271731a7f2ee803e40e97
Jack Muir <jmuirbusiness@gmail.com>
2023-10-04 18:50:05 +0100

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
@@ -23,3 +24,22 @@ ReactDOM.render(
 
 //This is the end result you're aiming for:
 //https://pogqj.csb.app/
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> 4a5986f (Initialize project using Create React App)
